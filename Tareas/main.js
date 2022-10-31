59f542214ae87da9a7b537a8a2ad0b533472ab77
Adrián Zúñiga Pérez <105097849+4Driian@users.noreply.github.com>
2022-10-31 10:17:54 -0600

    Se crea archivo y ejercicio1

---
+++ Tareas/main.js
@@ -0,0 +1,30 @@
+function main() {
+  console.log('Ejercicio 1 - ' +  identificador(1) +' Y '+ divisible(1));
+}
+
+/* Ejercicio 1 */
+function identificador(num){
+  if(num % 2 == 0 && num % 3 === 0) {
+    return 'El número es par'
+  } else {
+    return 'El número es impar'
+  }
+}
+
+function divisible(num2) { 
+  if(num2 % 3 == 0){
+    return 'divisible entre 3.'
+  }else if(num2 % 5 == 0){
+    return 'divisible entre 5.'
+  }else{
+    return 'NO es divisible entre 3 o 5.';
+  }
+}
+
+/* Ejercicio 2 */
+function suma (num3)
+for(var i = 0; i < suma; i++) {
+  
+}
+
+main();
