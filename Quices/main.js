b83346185eba1babed79d4cf5e8c82f50bb18254
Adrián Zúñiga Pérez <105097849+4Driian@users.noreply.github.com>
2022-11-10 20:39:19 -0600

    Se agregan cambios en el código

--- Quices/main.js
+++ Quices/main.js
@@ -1,4 +1,7 @@
-const numeros = [1,2,3,6,9,8]
+
+//let numeros = [1,2,3,4,4,5,6]
+
+let numeros = process.argv.slice(2)
 let numeroMayor = numeros[0];
 let numeroMenor = numeros[0];
 let numeroPromedio = 0;
@@ -6,12 +9,40 @@ let numMedio = 0
 let numEnMedio = 0;
 
 function resultados() {
-  console.log('El numero mayor es: ', numMayor());
-  console.log('El número menor es: ', numMenor());
   console.log('El promedio de los números es: ', numPromedio());
-  console.log('------------------------------------');
-  console.log('En los números: ', numeros);
-  console.log('La mediana es ', mediana());
+  console.log('La mediana es ', Number(mediana()));
+  console.log('El número menor es: ', Number(numMenor()));
+  console.log('El numero mayor es: ',  Number(numMayor()));
+}
+
+/* Numero promedio */
+function numPromedio() { 
+  for (i = 0; i < numeros.length;i++) {
+    numeroPromedio += numeros[i];
+  }
+resultado = numeroPromedio / numeros.length;
+  return resultado;
+}
+
+/* Mediana */
+function mediana () {
+  numEnMedio = Math.floor(numeros.length / 2)
+    if (numeros.length % 2 == 0) {
+      numMedio = (numeros[numEnMedio - 1 ] + numeros[numEnMedio]) / 2;
+    }else {
+      numMedio = numeros[Math.floor(numEnMedio)];
+    }
+    return numMedio
+  }
+
+/* Numero Menor */
+function numMenor() {
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] < numeroMenor) {
+      numeroMenor = numeros[i];
+    }
+  }
+return numeroMenor
 }
 
 /* Numero Mayor */
@@ -22,16 +53,6 @@ function resultados() {
       }
     }
   return numeroMayor 
-  }
-
-/* Numero Menor */
-  function numMenor() {
-    for (let i = 0; i < numeros.length; i++) {
-      if (numeros[i] < numeroMenor) {
-        numeroMenor = numeros[i];
-      }
-    }
-  return numeroMenor
   }
 resultados()
 
